fb5efc6cb506608ee337af6f24454bbb9afebe1a
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2024-03-28 20:24:59 -0500

    fix: Result override

--- overrides/src/components/Result.tsx
+++ overrides/src/components/Result.tsx
@@ -1,6 +1,32 @@
 "use client";
 import React from "react";
+import { useRouter } from "next/router";
+import { Heading, Text, Link, Flex, Divider, Skeleton } from "@chakra-ui/react";
 
-export default function Result() {
-  return 'This is an override.';
+import { ChevronLeftIcon } from "@chakra-ui/icons";
+
+export default function Result({
+  entry,
+  isLoading,
+}: {
+  entry: any;
+  isLoading: boolean;
+}) {
+  const router = useRouter();
+  return (
+    <>
+      <Link onClick={() => router.back()}>
+        <Flex alignItems="center" mb={4}>
+          <ChevronLeftIcon /> <Text fontSize="sm">Back</Text>
+        </Flex>
+      </Link>
+      <Divider my={2} />
+      <Skeleton isLoaded={!isLoading}>
+        <Heading as="h1" size="md" color="brand">
+          {entry.title}
+        </Heading>
+      </Skeleton>
+      This is an example of a component override.
+    </>
+  );
 }
